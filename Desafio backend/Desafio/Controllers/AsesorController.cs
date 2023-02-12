0a3e8f58fef4400eb8bef437053e86d271f0df93
SandroRamos <sandro271295@gmail.com>
2023-02-12 16:07:52 -0500

    Inicio Backend

---
+++ Desafio backend/Desafio/Controllers/AsesorController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Desafio.Models;
+
+namespace Desafio.Controllers
+{
+    [ApiController]
+    [Route("asesor")]
+    public class AsesorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarAsesor()
+        {
+            //todo el código
+            List<Asesor> asesores = new List<Asesor>
+            {
+                new Asesor
+                {
+                    id = 1,
+                    correo = "aessor@gmail.com",
+                    edad = "19",
+                    nombre = "Sandro Ramos"
+                },
+                new Asesor
+                {
+                    id = 2,
+                    correo = "aessor2@gmail.com",
+                    edad = "29",
+                    nombre = "Sandro Saravia"
+                }
+            };
+
+            return asesores;
+                                    
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarAsesor(Asesor asesor)
+        {
+            asesor.id = 3;
+
+            return new
+            {
+                success = true,
+                message = "asesor registrado",
+                result = asesor
+            };
+        }
+
+        [HttpGet]
+        [Route("listarxid")]
+        public dynamic listarAsesorxid( int _id)
+        {
+            //obtienes el asesor de la bd
+            
+
+            return new Asesor
+            {
+                id = _id,
+                correo = "aessor@gmail.com",
+                edad = "19",
+                nombre = "Sandro Ramos"
+            };
+
+        }
+
+        [HttpPost]
+        [Route("eliminar")]
+        public dynamic eliminarAsesor(Asesor asesor)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+            //eliminas en la db
+
+            if(token != "sandro123.")
+            {
+                return new
+                {
+                    succes = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+            
+            return new
+            {
+                success = true,
+                message = "asesor eliminado",
+                result = asesor
+            };
+        }
+
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ Desafio backend/Desafio/Models/Asesor.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio.Models
+{
+    public class Asesor
+    {
+        public string nombre { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+        public int id { get; set; }
+    }
+}
